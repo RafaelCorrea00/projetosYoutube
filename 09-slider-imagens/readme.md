a33c2da89c6f55276a45753afc01bc9236a972d0
Rafael Correa <contato.mundustech@gmail.com>
2022-10-09 16:12:54 -0300

    Commit
    Slider Funcional de Imagens
    Feito com HTML, CSS e JavaScript

---
+++ 09-slider-imagens/readme.md
@@ -0,0 +1,5 @@
+# Slider com HTML, CSS e JavaScript
+
+- Como criar um Slider funcional de forma simples com HTML, CSS e JavaScript
+
+[Sujeito Programador](https://www.youtube.com/watch?v=BpzyuuPIEaQ&list=PLn-1oXF21q6IwN9F3qZF9-2yEpkAtjU9w&index=2&t=1s)
---
+++ 09-slider-imagens/script.js
@@ -0,0 +1,14 @@
+let count = 1
+document.getElementById("radio1").checked = true
+
+setInterval(() => {
+    nextImage()
+}, 5000);
+
+function nextImage() {
+    count++
+    if (count > 4) {
+        count = 1
+    }
+    document.getElementById("radio" + count).checked = true
+}
